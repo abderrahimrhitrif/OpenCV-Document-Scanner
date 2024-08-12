a0c3a5794fac619fcbd9b14d429833331e9bccc3
Abderrahim Rhitrif <rhitrif.abderrahim@gmail.com>
2024-08-12 16:44:51 +0100

    Add a way to create Scanned directory

--- scanner.py
+++ scanner.py
@@ -56,6 +56,8 @@ warped_gray[mask] = 255
 
 timestamp = time.strftime("%Y%m%d_%H%M%S")
 output_dir = './Scanned'
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 output_filename = f'scanned_{timestamp}.jpg'
 output_path = os.path.join(output_dir, output_filename)
 
